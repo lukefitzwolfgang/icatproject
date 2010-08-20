960a55f60d9f4f7c86c2b35e0b4541d3830cf7bb
srikanthnag <srikanthnag@e4f78e02-9302-11dd-9f95-956597c7a372>
2010-08-20 08:13:04 +0000

    Update issue 26 refactoring of imports for parameter search

--- icat3_api/branches/parameters/icat3-exposed/src/java/uk/icat3/sessionbeans/search/InvestigationSearchLocal.java
+++ icat3_api/branches/parameters/icat3-exposed/src/java/uk/icat3/sessionbeans/search/InvestigationSearchLocal.java
@@ -1,8 +1,6 @@
 package uk.icat3.sessionbeans.search;
 
-import fr.ill.parametersearch.ParameterComparator;
-import fr.ill.parametersearch.ParameterOperable;
-import fr.ill.parametersearch.exception.ParameterSearchException;
+
 import java.util.Collection;
 import java.util.List;
 import javax.ejb.Local;
@@ -10,6 +8,9 @@ import uk.icat3.entity.IcatRole;
 import uk.icat3.entity.Investigation;
 import uk.icat3.entity.Parameter;
 import uk.icat3.exceptions.SessionException;
+import uk.icat3.parametersearch.ParameterComparator;
+import uk.icat3.parametersearch.ParameterOperable;
+import uk.icat3.parametersearch.exception.ParameterSearchException;
 import uk.icat3.search.AdvancedSearchDetails;
 import uk.icat3.search.KeywordDetails;
 import uk.icat3.util.InvestigationInclude;
