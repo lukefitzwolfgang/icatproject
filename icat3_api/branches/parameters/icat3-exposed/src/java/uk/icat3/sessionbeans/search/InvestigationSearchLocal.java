5fa249da14dc0c24cb2c2a5c4f898b55fd396dce
srikanthnag <srikanthnag@e4f78e02-9302-11dd-9f95-956597c7a372>
2010-08-26 13:44:53 +0000

    Update issue 26 refactoring  parameter search classes

--- icat3_api/branches/parameters/icat3-exposed/src/java/uk/icat3/sessionbeans/search/InvestigationSearchLocal.java
+++ icat3_api/branches/parameters/icat3-exposed/src/java/uk/icat3/sessionbeans/search/InvestigationSearchLocal.java
@@ -8,9 +8,9 @@ import uk.icat3.entity.IcatRole;
 import uk.icat3.entity.Investigation;
 import uk.icat3.entity.Parameter;
 import uk.icat3.exceptions.SessionException;
-import uk.icat3.parametersearch.ParameterComparator;
-import uk.icat3.parametersearch.ParameterOperable;
-import uk.icat3.parametersearch.exception.ParameterSearchException;
+import uk.icat3.search.parameter.ParameterComparisonCondition;
+import uk.icat3.search.parameter.ParameterCondition;
+import uk.icat3.exception.ParameterSearchException;
 import uk.icat3.search.AdvancedSearchDetails;
 import uk.icat3.search.KeywordDetails;
 import uk.icat3.util.InvestigationInclude;
@@ -57,7 +57,7 @@ public interface InvestigationSearchLocal {
 
     Collection<String> listInvestigationTypes(String sessionId) throws SessionException;
 
-    Collection<Investigation> searchByParameterOperable (String sessionId, ParameterOperable parameterOperable) throws SessionException, ParameterSearchException;
+    Collection<Investigation> searchByParameterOperable (String sessionId, ParameterCondition parameterOperable) throws SessionException, ParameterSearchException;
 
-    Collection<Investigation> searchByParameter(String sessionId, List<ParameterComparator> parameterComparator) throws SessionException, ParameterSearchException;
+    Collection<Investigation> searchByParameter(String sessionId, List<ParameterComparisonCondition> parameterComparator) throws SessionException, ParameterSearchException;
     }
