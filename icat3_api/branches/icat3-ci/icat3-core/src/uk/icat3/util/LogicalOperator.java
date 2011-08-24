7d0b5b8150c85f42aa185bbddc8d878ea3c4123e
mariusz.balawajder@gmail.com <mariusz.balawajder@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2011-08-24 12:30:17 +0000

    Created branch for CI modifications

---
+++ icat3_api/branches/icat3-ci/icat3-core/src/uk/icat3/util/LogicalOperator.java
@@ -0,0 +1,28 @@
+/*
+ * LogicalOperator.java
+ *
+ * Created on 27 February 2007, 10:35
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package uk.icat3.util;
+
+/**
+ * Type of keyword search.
+ *
+ * @author gjd37
+ */
+public enum LogicalOperator {
+    /**
+     * Match all of the keywords
+     */
+    AND, 
+    /**
+     * Match atleast one
+     */
+    OR;
+
+}
+
