ba33f04988e29303ca0bc88d6636e88209701537
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2012-01-31 15:39:12 +0000

    added information to icat40 examples to handle the firewall at RAL

--- ops/icat40/test_icat/test_one.sh
+++ ops/icat40/test_icat/test_one.sh
@@ -2,6 +2,7 @@
 #
 # $Id$
 #
+#java -Dhttp.proxyHost=wwwcache.rl.ac.uk -Dhttp.proxyPort=8080 -cp icat_api_examples.jar:../icat-client-1583.jar uk.icat.examples.$*
 java -cp icat_api_examples.jar:../icat-client-1583.jar uk.icat.examples.$*
 #
 # - the end - 
