e6b0d006f3855687912a35ee6d64b209cba89777
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2012-10-16 18:08:37 +0000

    updated the readme file and the glassfish.props to use the derby database

---
+++ ops/icat42/usertable_init/passwd.sql
@@ -0,0 +1,3 @@
+connect 'jdbc:derby://localhost:1527/icatuser;Password=APP:User=APP';
+INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD) VALUES ('root', 'password');
+
--- ops/icat42/usertable_init/usertable.sh
+++ ops/icat42/usertable_init/usertable.sh
@@ -1,6 +1,2 @@
-sqlplus icat42user/myicat42userpasswd@localhost <</eof
-INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD)
-               VALUES ('icat42', 'icat42passwd');
-exit
-/eof
+ij passwd.sql
 
