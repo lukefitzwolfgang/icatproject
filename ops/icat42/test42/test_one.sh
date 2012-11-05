b35cbc89a703ed149d43190531cbd1f27d06fbda
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2012-11-05 23:20:54 +0000

    updated test42 to support certificates

--- ops/icat42/test42/test_one.sh
+++ ops/icat42/test42/test_one.sh
@@ -1,9 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 #
 # $Id$
 #
-export method=Authenticate
-java -Djavax.net.ssl.trustStore=jssecacerts-www.icatproject.org -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$method
+export             method=Authenticate
+export         properties='example.properties'
+export        trust_store=jssecacerts-www.icatproject.org
+#
+# The following code is really horrible.  Unless you understand it, do not change it!  It determines the name of the file for the trusted certificate
+#
+wsdl=`grep ^wsdl ${properties}`
+front=${wsdl#*=};  back=${front%/*}; url=${back%/*}
+file=${url/#https:\/\//_};  
+file=${file//./_}
+file=${file//:/_}
+[ "${file:0:1}" = "_" -a -f $file ] && trust_store=$file
+java -Djavax.net.ssl.trustStore=$trust_store -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$method
+
+#java -Djavax.net.ssl.trustStore=$trust_store -Dhttp.proxyHost=wwwcache.rl.ac.uk -Dhttp.proxyPort=8080 -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$method
 #
 # - the end - 
 #
