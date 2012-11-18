34b186c5f13add4a60ae005bed104de09d60b7b2
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2012-11-18 17:23:18 +0000

    added elapse time to the information logged for test42

--- ops/icat42/test42/test_all.sh
+++ ops/icat42/test42/test_all.sh
@@ -13,9 +13,11 @@ while read icat
 do
   ./change_icat.sh $icat
   date="`date $date_format`"
-  test=`./test_one.sh`
+  test=`/usr/bin/time -f %e ./test_one.sh 2>time`
+  time=`cat time`
+  rm time
   wsdl=`grep ^wsdl $example_properties`
-  echo $date, $host, $base, $icat, $test, $wsdl
+  echo $date, $host, $base, $icat, $test, $wsdl, $time
   rm $example_properties
 done
 #
