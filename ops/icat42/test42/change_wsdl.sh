d94227ecfa009224b5be465192aacfabd5ea24d0
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2012-11-22 18:35:52 +0000

    updated the test42 scripts to ensure an appropriate response for failure to connect

--- ops/icat42/test42/change_wsdl.sh
+++ ops/icat42/test42/change_wsdl.sh
@@ -2,6 +2,15 @@
 #
 # $Id$
 #
+# This procedure accepts an argument with the url for a web service and transforms it
+#
+# For example:
+# ./change_wsdl.sh wsdl.location=https://wwws.esrf.fr/icat/ICATService/ICAT?wsdl
+#
+# returns:
+#
+# _wwws_esrf_fr
+#
 # The following code is really horrible.  Unless you understand it, do not change it!  It determines the name of the file for the trusted certificate
 #
 wsdl=$1
