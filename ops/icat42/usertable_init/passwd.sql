d759959514e98e67668bda237344b1c4821b64a7
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2012-11-29 19:10:39 +0000

    added svn:keywords Id to the files in icat421

--- ops/icat42/usertable_init/passwd.sql
+++ ops/icat42/usertable_init/passwd.sql
@@ -1,3 +1,5 @@
 connect 'jdbc:derby://localhost:1527/icatuser;Password=APP:User=APP';
-INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD) VALUES ('root', 'password');
+INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD) VALUES ('guest', 'password');
+INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD) VALUES ('reader', 'password');
+INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD) VALUES ('CIC', 'password');
 
