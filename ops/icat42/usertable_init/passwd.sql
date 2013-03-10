bc09bbd448eda834d911f0ca087d26a9b0158537
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2013-03-10 16:31:12 +0000

    added an additional user to the passwd table

--- ops/icat42/usertable_init/passwd.sql
+++ ops/icat42/usertable_init/passwd.sql
@@ -2,4 +2,5 @@ connect 'jdbc:derby://localhost:1527/icatuser;Password=APP:User=APP';
 INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD) VALUES ('guest', 'password');
 INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD) VALUES ('reader', 'password');
 INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD) VALUES ('CIC', 'password');
+INSERT INTO PASSWD (USERNAME, ENCODEDPASSWORD) VALUES ('root', 'password');
 
