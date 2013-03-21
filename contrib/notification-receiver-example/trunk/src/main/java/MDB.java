1a48534c949a513ce14bd4b2130856e4079ec05b
dr.s.m.fisher@gmail.com <dr.s.m.fisher@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2013-03-21 14:11:49 +0000

    Check in of first version

---
+++ contrib/notification-receiver-example/trunk/src/main/java/MDB.java
@@ -0,0 +1,39 @@
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+/*
+ * The MessageDriven annotation defines a message selector with an SQL like syntax to select only datafile creates.
+ */
+@MessageDriven(mappedName = "jms/ICATTopic", activationConfig = { @ActivationConfigProperty(propertyName = "messageSelector", propertyValue = "entity = 'DATAFILE' AND operation='CREATE'") })
+public class MDB implements MessageListener {
+
+	/**
+	 * This is a singleton as the container may create many instances of a message driven bean and
+	 * we choose to have one instance dealing with all messages. In this case it is not
+	 * necessary however if we wish to deal with create and delete of data files then having one
+	 * instance with an overview is preferable.
+	 */
+	static private final DelayedAction delayedAction = DelayedAction.INSTANCE;
+
+	@Override
+	public void onMessage(Message message) {
+
+		ObjectMessage om = (ObjectMessage) message;
+		try {
+			/* The values of entity and operation are constrained by the message selector */
+			String operation = om.getStringProperty("operation");
+			String entity = om.getStringProperty("entity");
+			Long id = (Long) om.getObject();
+			delayedAction.note(operation, entity, id);
+
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
