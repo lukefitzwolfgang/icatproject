9d0f2a32d0cb96a6aad7ec5115b9cf52f05d674d
crcamills@googlemail.com <crcamills@googlemail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2013-04-25 18:15:29 +0000

    added materials for SV4

--- ops/icat42/test424/test_2.sh
+++ ops/icat42/test424/test_2.sh
@@ -2,15 +2,8 @@
 #
 # $Id$
 #
-export         properties='example.properties'
-export        trust_store=jssecacerts-www.icatproject.org
-#
-wsdl=`grep ^wsdl ${properties}`
-file=`./change_wsdl.sh $wsdl`
-[ "${file:0:1}" = "_" -a -f $file ] && trust_store=$file
-
-#java -Djavax.net.ssl.trustStore=$trust_store                                                          -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
-java -Djavax.net.ssl.trustStore=$trust_store -Dhttp.proxyHost=wwwcache.rl.ac.uk -Dhttp.proxyPort=8080 -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
+java                                                           -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
+#java -Dhttp.proxyHost=wwwcache.rl.ac.uk -Dhttp.proxyPort=8080 -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
 #
 # - the end - 
 #
