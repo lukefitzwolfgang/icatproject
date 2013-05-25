1624f6d7df384683234c0a8ea4a873cbeeb4dd36
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2013-05-25 14:37:43 +0000

    added premain to the tests for test421

---
+++ ops/icat42/test421/premain.java
@@ -0,0 +1,18 @@
+import java.lang.instrument.*;
+import java.net.URL;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+public class premain {
+    static { 
+        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier( new javax.net.ssl.HostnameVerifier(){
+            public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
+                return true;
+            }
+        });
+    }
+
+    public static void premain(String args, Instrumentation inst) {
+        //System.out.println("Ignore Host Certificate Error") ;
+    }
+}
