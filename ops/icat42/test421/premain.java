5456e2606fdd56bf4772fea59903022039facb19
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2013-05-26 22:04:39 +0000

    made cosmetic changes to premain in test421

--- ops/icat42/test421/premain.java
+++ ops/icat42/test421/premain.java
@@ -3,15 +3,17 @@ import java.net.URL;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
+// $Id$
+
 public class premain {
-    static { 
-        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier( new javax.net.ssl.HostnameVerifier(){
+    static {
+        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
+        new javax.net.ssl.HostnameVerifier () {
             public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
                 return true;
             }
         });
     }
-
     public static void premain(String args, Instrumentation inst) {
         //System.out.println("Ignore Host Certificate Error") ;
     }
