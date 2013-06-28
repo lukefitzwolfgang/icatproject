0fce1d261d618602c435e235006c0157b8150047
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2013-06-28 13:41:57 +0000

    corrected proxy settings in test_2.sh

--- ops/icat42/test425/test_2.sh
+++ ops/icat42/test425/test_2.sh
@@ -9,9 +9,9 @@ wsdl=`grep ^wsdl ${properties}`
 file=`./change_wsdl.sh $wsdl`
 [ "${file:0:1}" = "_" -a -f $file ] && trust_store="-Djavax.net.ssl.trustStore=$file"
 #set -x
-#java $trust_store                                                          -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
-java $trust_store  -Dhttp.proxyHost=wwwcache.rl.ac.uk  -Dhttp.proxyPort=8080 \
-                   -Dhttps.proxyHost=wwwcache.rl.ac.uk -Dhttps.proxyPort=8080 -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
+java $trust_store                                                          -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
+#java $trust_store  -Dhttp.proxyHost=wwwcache.rl.ac.uk  -Dhttp.proxyPort=8080 \
+#                   -Dhttps.proxyHost=wwwcache.rl.ac.uk -Dhttps.proxyPort=8080 -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
 #
 # - the end - 
 #
