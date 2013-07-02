dded7a6a681f3fc0f54a960e4b8e36ff09f3dc89
crcamills@gmail.com <crcamills@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2013-07-02 13:33:42 +0000

    changed test_2 for tng-1 to support Glassfish 4 behind an Apache with ssl

--- ops/icat42/test421/test_2.sh
+++ ops/icat42/test421/test_2.sh
@@ -3,14 +3,14 @@
 # $Id$
 #
 export         properties='example.properties'
-export        trust_store=jssecacerts-www.icatproject.org
+export        trust_store=
 #
 wsdl=`grep ^wsdl ${properties}`
 file=`./change_wsdl.sh $wsdl`
-[ "${file:0:1}" = "_" -a -f $file ] && trust_store=$file
+[ "${file:0:1}" = "_" -a -f $file ] && trust_store="-Djavax.net.ssl.trustStore=$file"
 
-java -javaagent:premain.jar -Djavax.net.ssl.trustStore=$trust_store -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
-#java -javaagent:premain.jar -Djavax.net.ssl.trustStore=jssecacerts -Dhttp.proxyHost=wwwcache.rl.ac.uk -Dhttp.proxyPort=8080 -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
+ java -javaagent:premain.jar $trust_store                                                          -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
+#java -javaagent:premain.jar $trust_store -Dhttp.proxyHost=wwwcache.rl.ac.uk -Dhttp.proxyPort=8080 -cp icat_api_examples.jar:icat-client.jar uk.icat.examples.$*
 #
 # - the end - 
 #
