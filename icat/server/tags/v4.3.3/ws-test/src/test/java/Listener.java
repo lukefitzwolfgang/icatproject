53bf7d7c9816c3da8856cc4cb1f65eb9aca346ca
dr.s.m.fisher@gmail.com <dr.s.m.fisher@gmail.com@e4f78e02-9302-11dd-9f95-956597c7a372>
2014-07-09 09:33:30 +0000

    [maven-release-plugin]  copy for tag v4.3.3

---
+++ icat/server/tags/v4.3.3/ws-test/src/test/java/Listener.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+public class Listener implements MessageListener {
+
+	Queue<ObjectMessage> q = new LinkedList<ObjectMessage>();
+
+	@Override
+	public void onMessage(Message message) {
+		synchronized (q) {
+			q.add((ObjectMessage) message);
+		}
+	}
+
+	public ObjectMessage getMessage() {
+		synchronized (q) {
+			return q.poll();
+		}
+	}
+
+}
